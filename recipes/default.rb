34226b99d35dee732675831609580049d0b54554
Thorsten Fischer <thorsten@froschi.org>
2012-06-20 14:54:42 +0200

    Added dependencies for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,30 @@
+include_recipe "libbz2"
+include_recipe "libdb"
+include_recipe "libedit"
+include_recipe "libmagic"
+include_recipe "libpcre"
+include_recipe "libssl"
+include_recipe "libxml2"
+include_recipe "mime-support"
+include_recipe "php5-common"
+include_recipe "tzdata"
+include_recipe "ucf"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libcomerr"
+  include_recipe "libgssapi-krb5"
+  include_recipe "libkrb5"
+  include_recipe "libncurses"
+
   packages |= %w/
-    php5-cli
+    php5-cgi
   /
 when "precise"
   packages |= %w/
-    php5-cli
+    php5-cgi
   /
 end
 
